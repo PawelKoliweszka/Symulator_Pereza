1b33e6beb45ce2ca0e6745f07a7d0351bf619b45
PawelKoliweszka <pawelgracz@onet.pl>
2019-06-05 22:33:37 +0200

    nieudane zapytanie 2

---
+++ symulatorPereza/src/app/rejestracja/rejestracja.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, post } from 'selenium-webdriver/http';
+
+@Component({
+  selector: 'app-rejestracja',
+  templateUrl: './rejestracja.component.html',
+  styleUrls: ['./rejestracja.component.css']
+})
+export class RejestracjaComponent implements OnInit {
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+  }
+
+  daneToServer(name: string, password1: string, password2: string, email: string) {
+    return this.http.post('localhost:3001/rejestracja',
+    {
+      'nick' : name,
+      'password1' : password1,
+      'password2' : password2,
+      'email' : email
+    });
+  }
+}
